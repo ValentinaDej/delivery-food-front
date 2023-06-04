cc030ee4191182357e3960e74af46d7f274aae04
ValentinaDej <safonikv@gmail.com>
2023-06-04 10:02:09 +0300

    Update instance.js

--- src/shared/services/instance.js
+++ src/shared/services/instance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://deliveryfoodsevice.onrender.com/api',
+  baseURL: 'https://delivery-food-back-yqm5.onrender.com/api',
   //baseURL: 'http://localhost:5000/api',
 });
 
